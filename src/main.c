5d507dc8c8abb787427c7f2e432623d6b8b6e6c4
RamonRuizPe <rualejandro.ra@gmail.com>
2022-10-11 21:17:54 -0500

    Cases has been added to algorithm

--- src/main.c
+++ src/main.c
@@ -35,11 +35,13 @@ struct hist_type
 struct hist_type hist[HIST_MAX];
 size_t hist_sz = 0;
 #define hist_top (hist_sz - 1)
+#define hist_curr (hist[hist_top])
 
 #define SENT_MAX 64
 char sent[SENT_MAX];
 size_t sent_sz = 0;
 #define sent_top (sent_sz - 1)
+#define sent_tchar (sent[sent_top])
 
 enum state_enum
 {
@@ -55,6 +57,8 @@ int parse(char *);
 
 int lhs_compare(const char *, const struct lhs_type *);
 int lhs_find(const char *);
+void sent_insert(const char *);
+void print_state(int, const char*);
 
 int main(int argc, char **argv)
 {
@@ -70,9 +74,14 @@ int main(int argc, char **argv)
     }
 
     load_grammar(fin);
-    print_lhs();
-    parse(argv[1]);
-
+    //print_lhs();
+    int result = parse(argv[1]);
+    if(result == 0){
+        puts("String has been accepted");
+    }else{
+        //Ñao ñao amigao
+        puts("String has not been accepted");
+    }
     return 0;
 }
 
@@ -110,13 +119,14 @@ int parse(char *T)
     };
     memcpy(sent, (char[2]){'\0', lhs[0].nt}, 2 * sizeof(char));
     sent_sz += 2;
-
+    int loop = 1;
     /// 2. loop
     while (1)
     {
+        print_state(idx, T);
         /// 3. get stack-top elements and determine configuration
-        struct hist_type hist_curr = hist[hist_top];
-        char sent_tchar = sent[sent_top];
+        //struct hist_type hist_curr = hist[hist_top];
+        //char sent_tchar = sent[sent_top];
         if (state == PARSE && idx == t_len && sent_top == '\0')
         {
             conf_case = 3;
@@ -163,7 +173,7 @@ int parse(char *T)
                         if (idx == 0 && hist_curr.symb == lhs[0].nt)
                         {
                             // unsuccessful parse
-                            return 1;
+                            return EXIT_FAILURE;
                         }
                         else
                         {
@@ -175,8 +185,65 @@ int parse(char *T)
         }
 
         /// 4. select case
+        size_t rhs_idx;
+        size_t rhs_len;
+        printf("%d. Using case %d \n", loop++ , conf_case);
+        switch (conf_case)
+        {
+        case 1:
+            //Tree expansion (First production of a nonterminal)
+            hist_sz++;
+            hist_curr.p_num = 1;
+            hist_curr.symb = sent_tchar;
+            rhs_idx = lhs[lhs_find(&sent_tchar)].first;
+            sent_sz--;
+            sent_insert(rhs[rhs_idx]);
+            break;
+        case 2:
+            //Input symbol match (Advance the index into the input string)
+            hist_sz++;
+            hist_curr.p_num = 0;
+            hist_curr.symb = sent_tchar;
+            idx++;
+            sent_sz--;
+            break;
+        case 3:
+            //Terminate (Successful parse has been performed)
+            state = TERMINATION;
+            return EXIT_SUCCESS;
+        case 4:
+            //Input symbol mismatch (A generated substring failed to match the input)
+            state = BACKTRACK;
+            break;
+        case 5:
+            //Backtrack on input
+            idx--;
+            hist_sz--;
+            sent[sent_sz++] = hist_curr.symb;
+            break;
+        case 6:
+            //6a. Next alternate, replace with next production
+            state = PARSE;
+            rhs_idx = lhs[lhs_find(&(hist_curr.symb))].first + (hist_curr.p_num - 1);
+            rhs_len = strlen(rhs[rhs_idx]);
+            sent_sz -= rhs_len;
+            hist_curr.p_num++;
+            sent_insert(rhs[rhs_idx + 1]);
+            break;
+        case 7:
+            //6c. the alternates for A has been exhausted; remove Aj)
+            rhs_idx = lhs[lhs_find(&(hist_curr.symb))].first + (hist_curr.p_num - 1);
+            rhs_len = strlen(rhs[rhs_idx]);
+            sent_sz -= rhs_len;
+            sent[sent_sz++] = hist_curr.symb;
+            hist_sz--;
+            break;
+        default:
+            //You shouldnt be here
+            return EXIT_FAILURE;
+        }
     }
-
+    
     return 0;
 }
 
@@ -185,6 +252,49 @@ int lhs_compare(const char *key, const struct lhs_type *elem)
     // returns 0 if equal
     return !(*key == elem->nt);
 }
+
+void sent_insert(const char *key){
+    size_t key_len = strlen(key);
+    //size_t j = 0;
+    for (int i = key_len - 1; i >= 0; i--)
+    {
+        sent[sent_sz++] = key[i];
+    }
+}
+
+void print_state(int idx, const char* key){
+    char state_c;
+    switch (state)
+    {
+    case PARSE:
+        state_c = 'q';
+        break;
+    case BACKTRACK:
+        state_c = 'b';
+        break;
+    case TERMINATION:
+        state_c = 't';
+        break;
+    }
+    printf("[ %c ; %d ; ", state_c, idx);
+    for (size_t i = 0; i < hist_sz; i++)
+    {
+        if( hist[i].p_num != 0){
+            printf("%c%d ", hist[i].symb, hist[i].p_num);
+        }else{
+            printf("%c ", hist[i].symb);
+        }
+        
+    }
+    printf("; ");
+    for (int i = sent_top; i > 0; i--)
+    {
+        printf("%c", sent[i]);
+    }
+    printf(" ] %s \n", (key+idx));
+    
+}
+
 
 /**
  * Find non-terminal character in lhs
